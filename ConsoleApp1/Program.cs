dcb93c815f4fedd9b89456a58452594d9fbf04f9
Artrit0chka <58296372+Artrit0chka@users.noreply.github.com>
2022-12-23 01:59:33 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hello, World!");
+int a = 1; //1
+int b = 1; //2
+
+for (int i = 0; i < 8; i++){
+    (a,b) = (b, a+b);
+}
+//2,3
+//3,4
+//5,6
+//6,7
+//7,8
+//8,9
+//9,10 = 55
+
+Console.WriteLine(b);
